18960c4a54aa56a783346cd04606d01ad81b8a37
lhamot <loic.hamot@yahoo.fr>
2023-09-14 22:14:31 +0200

    Add full tests for find_last_if_not

--- test/test_algo2.cpp
+++ test/test_algo2.cpp
@@ -540,7 +540,7 @@ struct test_find_last_
         auto r1 = make_test_view_adapter<CS, Tag, Sized>(in);
 
         {
-            COMPARE_DURATION_TO_STD_RANGES_23( // find_last does not exist in std
+            COMPARE_DURATION_TO_STD_RANGES_23(
                 "find_last",
                 range_type,
                 [&]
@@ -685,6 +685,73 @@ void test_find_last_if()
 
     foreach_range_combination<test_algo<test_find_last_if_>>();
 }
+template <CommonOrSent CS, typename Tag, bool Sized>
+struct test_find_last_if_not_
+{
+    template <bool = true>
+    void test()
+    {
+        RAH2_STD::vector<Coord> in{{1, 0}, {2, 0}, {3, 0}, {4, 0}, {3, 0}, {2, 0}};
+        auto r1 = make_test_view_adapter<CS, Tag, Sized>(in);
+        testSuite.test_case("range");
+        testSuite.test_case("noproj");
+        auto const iter = RAH2_NS::ranges::find_last_if_not(
+            r1,
+            [](Coord const& c) {
+                return c != Coord{3, 0};
+            });
+        testSuite.test_case("iter");
+        testSuite.test_case("proj");
+        assert((*RAH2_NS::ranges::begin(iter) == Coord{3, 0}));
+        assert((RAH2_NS::ranges::distance(iter.begin(), iter.end()) == 2));
+        auto const iter2 = RAH2_NS::ranges::find_last_if_not(
+            r1.begin(), r1.end(), [](intptr_t c) { return c != 3; }, &Coord::x);
+        assert((*RAH2_NS::ranges::begin(iter2) == Coord{3, 0}));
+        assert((RAH2_NS::ranges::distance(iter2.begin(), iter2.end()) == 2));
+        auto const iter3 = RAH2_NS::ranges::find_last_if_not(
+            r1.begin(), r1.end(), [](intptr_t c) { return c != 45; }, &Coord::x);
+        assert(RAH2_NS::ranges::empty(iter3));
+    }
+    template <bool = true>
+    void test_perf(char const* range_type)
+    {
+        RAH2_STD::vector<Coord> in(1000000, Coord{1, 2});
+        in.push_back(Coord{3, 4});
+        in.push_back(Coord{3, 4});
+        in.push_back(Coord{18, 4});
+        auto r1 = make_test_view_adapter<CS, Tag, Sized>(in);
+
+        {
+            COMPARE_DURATION_TO_STD_RANGES_23( // find_last_if_not does not exist in std
+                "find_last_if_not",
+                range_type,
+                [&]
+                {
+                    auto iter = STD::find_last_if_not(
+                        r1.begin(),
+                        r1.end(),
+                        [](Coord const& c) {
+                            return c != Coord{3, 4};
+                        });
+                    assert((*RAH2_NS::ranges::begin(iter) == Coord{3, 4}));
+                    assert((RAH2_NS::ranges::distance(iter.begin(), iter.end()) == 2));
+                });
+        }
+        {
+            COMPARE_DURATION_TO_STD_RANGES_23(
+                "find_last_if_not_proj",
+                range_type,
+                [&]
+                {
+                    auto iter = STD::find_last_if_not(
+                        r1.begin(), r1.end(), [](intptr_t c) { return c != 3; }, &Coord::x);
+                    assert((*RAH2_NS::ranges::begin(iter) == Coord{3, 4}));
+                    assert((RAH2_NS::ranges::distance(iter.begin(), iter.end()) == 2));
+                });
+        }
+    }
+    static constexpr bool do_test = RAH2_NS::derived_from<Tag, RAH2_NS::forward_iterator_tag>;
+};
 void test_find_last_if_not()
 {
     testSuite.test_case("sample");
@@ -715,6 +782,8 @@ void test_find_last_if_not()
         assert(i2.begin() == v.end());
     }
     /// [rah2::ranges::find_last_if_not]
+
+    foreach_range_combination<test_algo<test_find_last_if_not_>>();
 }
 void test_find_end()
 {
