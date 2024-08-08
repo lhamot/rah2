5d55b6e8313199e98e97cb1fd610e8c3bbc8e01d
lhamot <loic.hamot@yahoo.fr>
2024-08-08 14:28:27 +0200

    fix

--- test/test_algo3.cpp
+++ test/test_algo3.cpp
@@ -2531,6 +2531,7 @@ struct test_replace_if_
             RAH2_STD::vector<int> empty_out_;
             auto empty_out = make_test_view_adapter<CS, Tag, Sized>(empty_out_);
             CHECK(empty_out.begin() == empty_out.end());
+            CHECK(empty_out.empty());
 
             auto result3 = RAH2_NS::ranges::replace_if(
                 empty_out.begin(), empty_out.end(), [](auto v) { return v == 169; }, 0);
