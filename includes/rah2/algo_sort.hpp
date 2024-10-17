7fe46e1b31f2a646ef9f59fdb667497ce4108aef
lhamot <loic.hamot@yahoo.fr>
2024-10-17 14:38:57 +0200

    fix

--- includes/rah2/algo_sort.hpp
+++ includes/rah2/algo_sort.hpp
@@ -186,7 +186,7 @@ namespace RAH2_NS
                 {
                     auto pred_proj1_proj2 = details::wrap_pred_proj(
                         RAH2_STD::move(compare), RAH2_STD::move(proj1), RAH2_STD::move(proj2));
-                    (void*)&pred_proj1_proj2;
+                    (void)&pred_proj1_proj2;
                     auto pred_proj2_proj1 = details::wrap_pred_proj(
                         RAH2_STD::move(compare), RAH2_STD::move(proj2), RAH2_STD::move(proj1));
 
